80340f2e863ce12ea9f82b509a6c048d3046ed00
shakarr <risaaflo@gmail.com>
2022-09-29 17:24:16 +0200

    Add component: Navbar

--- src/app/application/application.module.ts
+++ src/app/application/application.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ApplicationComponent } from './application.component';
 
+import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { ApplicationRoutingModule } from './application-routing.module';
 
-import { HomeComponent } from '../components/home/home.component';
+import { HomeComponent } from '../pages/home/home.component';
 import { NavbarComponent } from '../components/navbar/navbar.component';
 
 @NgModule({
   declarations: [ApplicationComponent, HomeComponent, NavbarComponent],
-  imports: [CommonModule, ApplicationRoutingModule],
+  imports: [CommonModule, ApplicationRoutingModule, NgbTooltipModule],
 })
 export class ApplicationModule {}
