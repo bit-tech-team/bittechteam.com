1e7a3e18c614d2d99d1e178d7ea730440afb838e
shakarr <risaaflo@gmail.com>
2022-11-03 14:33:10 +0100

    remove the backend folder and move it to a new repo

--- backend/helpers/generateID.js
+++
@@ -1,8 +0,0 @@
-const generateID = () => {
-  const random = Math.random().toString(32).substring(2);
-  const date = Date.now().toString(32);
-
-  return random + date;
-};
-
-export default generateID;
