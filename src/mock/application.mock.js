f8aa912d09dc4c09d247d0f1e75078e96b19ff5f
shakarr <risaaflo@gmail.com>
2022-11-15 15:57:23 +0100

    added mock to optimize the code in the Application component

---
+++ src/mock/application.mock.js
@@ -0,0 +1,33 @@
+import imgAnimeTracker from "../assets/images/anime-tracker.png";
+import imgMGP from "../assets/images/mgp.png";
+
+export const APPLICATION = [
+  {
+    titleSection: "Anime Tracker, Desktop APP for Anime Scene Search by Image",
+    descriptionSection:
+      "Find your favorite anime through an image, either through a link or a local file, all this with 90% accuracy",
+    imageSection: imgAnimeTracker,
+    features: [
+      "90% accuracy",
+      "Search your anime by a link",
+      "Search your anime by an image on your computer",
+    ],
+  },
+  {
+    titleSection: "Magnus Project Manager",
+    descriptionSection:
+      "Magnus is a project manager that has many typical project features and an elegant design.",
+    imageSection: imgMGP,
+    features: [
+      "Create and manage your projects",
+      "Add and remove collaborators",
+      "Add, modify and delete your tasks",
+    ],
+  },
+  /*  {
+    titleSection: "Spender Manager",
+    descriptionSection: "",
+    imageSection: "",
+    features: ["asd", "asd", "asd", "asd", ""],
+  }, */
+];
